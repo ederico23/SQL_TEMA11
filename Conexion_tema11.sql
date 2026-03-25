203b1c1c12778a71e3a8a4d009e45e02fd813125
Eder <edergracia006@gmail.com>
2026-03-25 20:27:23 +0100

    clase 25/03/26

---
+++ Conexion_tema11.sql
@@ -0,0 +1,5 @@
+--CLASE
+
+/*1. Crear una vista DEP30 que contiene el APELLIDO, el OFICIO, y el SALARIO de los
+empleados de la tabla EMPLE del departamento 30. Después comprobar
+descripción y contenido.*/
